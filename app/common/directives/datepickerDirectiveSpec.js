f91897fccb4d8bcfebb581c5bfb1959c2dc3deb0
Michal Pietrzak <201011@student.pwr.wroc.pl>
2016-06-07 22:57:30 +0200

    Add missing unit test for datepickerDirective

--- app/common/directives/datepickerDirectiveSpec.js
+++ app/common/directives/datepickerDirectiveSpec.js
@@ -3,9 +3,19 @@
 "use strict";
 
 describe("Datepicker directive", function () {
+    var date, elm, scope;
+
     beforeEach(module("app.ng-date-picker"));
 
-    it("should ...", inject(function () {
-
-    }));
+    it('should put the date in the model', function () {
+        return inject(function ($compile, $rootScope) {
+            scope = $rootScope.$new();
+            date = new Date('2016-05-23T00:00:00.000Z');
+            elm = angular.element('<input data-ng-date-picker=\'\' data-ng-model=\'x\'></input>');
+            $compile(elm)(scope);
+            elm.datepicker('setDate', date);
+            $.datepicker._selectDate(elm);
+            expect(scope.x).toEqual('23/05/2016');
+        });
+    });
 });
