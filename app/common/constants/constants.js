25f0d0dc651432dff2d9c501540eed9fe16dc7d1
Michal Pietrzak <201011@student.pwr.wroc.pl>
2016-06-11 14:46:38 +0200

    Update app version - v0.0.2

--- app/common/constants/constants.js
+++ app/common/constants/constants.js
@@ -3,5 +3,5 @@
 angular.module('app')
 
         .constant('APP_NAME', 'AngularJS Template')
-        .constant('APP_VERSION', '0.0.1')
+        .constant('APP_VERSION', '0.0.2')
         .constant('APP_AUTHOR', 'Michal Pietrzak');
